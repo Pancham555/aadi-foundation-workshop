f961d91961a845ceae3ba604e47f276bde2dfb77
Pancham <{panchamb63@gmail.com}+{Pancham555}@users.noreply.github.com>
2024-07-03 12:20:39 +0530

    Made project 2

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/tropical-leaves-background-design-summer-leaves-flat-illustration-simple-banner-with-copy-space-free-vector.jpg";
+import image from "../images/top-img.jpg";
 
 const imageAltText = "General image background";
 
